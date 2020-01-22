608c33eec9941fecbf4b667729bd16194d91e44b
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-22 21:43:47 +0900

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by 原田悠嗣 on 2020/01/22.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
