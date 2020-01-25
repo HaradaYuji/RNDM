4f7c320b87467639448bdab2c10c8dfbb3207eca
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-25 20:55:47 +0900

    [Create] Setup tableview delegate

--- RNDM/Controller/MainVC.swift
+++ RNDM/Controller/MainVC.swift
@@ -16,12 +16,27 @@ enum ThoughtCategory: String {
 }
 
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // Outlets
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
 
