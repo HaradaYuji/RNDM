eaff93a0af38fade366e193c237a50d0c35af222
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-25 17:36:38 +0900

    [Create] Post Firestore

--- RNDM/Controller/AddThoughtVC.swift
+++ RNDM/Controller/AddThoughtVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class AddThoughtVC: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
@@ -14,19 +15,21 @@ class AddThoughtVC: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     // Outlets
     @IBOutlet weak var categorySegment: UISegmentedControl!
     @IBOutlet weak var userNameText: UITextField!
-    @IBOutlet weak var throughtText: UITextView!
+    @IBOutlet weak var thoughtText: UITextView!
     @IBOutlet weak var postBtn: UIButton!
 
+    // Variables
+    private var selectedCategory = "funny"
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         postBtn.layer.cornerRadius = 4
-        throughtText.layer.cornerRadius = 4
-        throughtText.text = "My random thought ..."
-        throughtText.textColor = .lightGray
+        thoughtText.layer.cornerRadius = 4
+        thoughtText.text = "My random thought ..."
+        thoughtText.textColor = .lightGray
 
-        throughtText.delegate = self
+        thoughtText.delegate = self
     }
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -40,10 +43,30 @@ class AddThoughtVC: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     }
 
     @IBAction func categoryChanged(_ sender: Any) {
+
+
+
     }
 
 
     @IBAction func postBtnTapped(_ sender: Any) {
+        Firestore.firestore().collection("thoughts").addDocument(data: [
+            "category" : selectedCategory,
+            "numComments" : 0,
+            "numLikes" : 0,
+            // - FIXME: ちゃんとアンラップしたい
+            "thoughtText" : thoughtText.text!,
+            "timestamp" : FieldValue.serverTimestamp(),
+            // - FIXME: ちゃんとアンラップしたい
+            "username" : userNameText.text!]) { (err) in
+            if let err = err {
+                debugPrint("Error adding document: \(err)")
+            } else {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+
+
 
 
 
