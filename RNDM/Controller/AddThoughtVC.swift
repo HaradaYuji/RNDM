baaacbe74510d367e3b669957df0ac0e0fcc1006
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-25 12:46:28 +0900

    [Create] Setup storyboard And Create New Groups

---
+++ RNDM/Controller/AddThoughtVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddThoughtVC.swift
+//  RNDM
+//
+//  Created by 原田悠嗣 on 2020/01/25.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class AddThoughtVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
