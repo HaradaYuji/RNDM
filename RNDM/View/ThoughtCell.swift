469cafa5cee22f51ad0487cb363ad21673b2d627
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-26 12:01:05 +0900

    [Create] Create ThoughtCell And Configure it

---
+++ RNDM/View/ThoughtCell.swift
@@ -0,0 +1,24 @@
+//
+//  ThoughtCell.swift
+//  RNDM
+//
+//  Created by 原田悠嗣 on 2020/01/26.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtCell: UITableViewCell {
+
+    // Outlets
+    
+
+
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
