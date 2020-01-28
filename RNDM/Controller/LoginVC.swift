97684af7185af4fedbaec57b72d0c743ec107c63
原田悠嗣 <bikkirisaburou@gmail.com>
2020-01-29 00:33:39 +0900

    [Create] Login VC

---
+++ RNDM/Controller/LoginVC.swift
@@ -0,0 +1,36 @@
+//
+//  LoginVC.swift
+//  RNDM
+//
+//  Created by 原田悠嗣 on 2020/01/29.
+//  Copyright © 2020 原田悠嗣. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+
+    @IBOutlet weak var emailText: UITextField!
+
+    @IBOutlet weak var passwordText: UITextField!
+
+
+    @IBOutlet weak var loginBtn: UIButton!
+
+    @IBOutlet weak var createUserBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func loginBtnTapped(_ sender: Any) {
+    }
+
+
+    @IBAction func createUserTapped(_ sender: Any) {
+    }
+    
+}
