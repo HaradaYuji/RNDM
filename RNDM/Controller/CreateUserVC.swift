87342fc1a641a684a7eeae92dbf48f5dc4624c8c
原田悠嗣 <bikkirisaburou@gmail.com>
2020-02-07 00:22:26 +0900

    [Create] UserCreate

--- RNDM/Controller/CreateUserVC.swift
+++ RNDM/Controller/CreateUserVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateUserVC: UIViewController {
 
@@ -24,25 +25,54 @@ class CreateUserVC: UIViewController {
 
 
 
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         createBtn.layer.cornerRadius = 10
         cancelBtn.layer.cornerRadius = 10
 
-
-
-
     }
     
-
     @IBAction func createTapped(_ sender: Any) {
-    }
 
+        guard let email = emailText.text,
+            let password = passwordText.text,
+            let username = usernameText.text else { return }
+
+
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+
+            if let error = error {
+                debugPrint("Error creating user: \(error.localizedDescription)")
+            }
+
+            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+            changeRequest?.displayName = username
+            changeRequest?.commitChanges(completion: { (error) in
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                }
+            })
+
+            guard let userId = Auth.auth().currentUser?.uid else { return }
+            Firestore.firestore().collection(USERS_REF).document(userId).setData([
+                USERNAME: username,
+                DATE_CREATED: FieldValue.serverTimestamp()
+                ], completion: { (error) in
+                    if let error = error {
+                        debugPrint(error.localizedDescription)
+                    } else {
+                        self.dismiss(animated: true)
+                    }
+            })
+        }
+    }
 
 
     @IBAction func cancelTapped(_ sender: Any) {
+        self.dismiss(animated: true)
     }
-
 
 }
